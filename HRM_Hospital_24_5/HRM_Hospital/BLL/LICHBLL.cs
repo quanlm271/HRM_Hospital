a53506e4b6f3371c8c84abbf5458df40252ca901
quanlm.271 <quanlm.271@gmail.com>
2015-05-24 17:23:27 +0700

    - Edit Ke thua - Sua lai che do xem lich (ViewByDate va ViewByMonth) - Them ham xoa

--- HRM_Hospital_24_5/HRM_Hospital/BLL/LICHBLL.cs
+++ HRM_Hospital_24_5/HRM_Hospital/BLL/LICHBLL.cs
@@ -10,9 +10,8 @@ using System.Windows.Forms;
 
 namespace HRM_Hospital.BLL
 {
-    class LICHBLL
+    class LICHBLL : BLL
     {
-        QLNSDataContext DB = new QLNSDataContext();
         public List<LICH> LayTatCa()
         {
             return DB.LICHes.ToList();
@@ -21,19 +20,37 @@ namespace HRM_Hospital.BLL
         {
             foreach (string lich in (from record in DB.LICHes select record.MALICH))
             {
-                if (lich.ToLower().Contains(Ma_Lich.ToLower()) && (lich.Trim().Length == Ma_Lich.Trim().Length ))
+                if (lich.ToLower().Contains(Ma_Lich.ToLower()) && (lich.Trim().Length == Ma_Lich.Trim().Length))
                 {
                     return true;
                 }
             }
             return false;
         }
-        public List<LICH> XemLich(string MaNV, DateTime dt)
+        public List<LICH> ViewByMonth(string MANV, int month)
         {
-            var LICH = DB.LICHes.Where(lich => lich.NGAY.Value == dt.Date && lich.MANV == MaNV);
+            var LICH = from table in DB.LICHes
+                       where (table.MANV == MANV && table.NGAY.Value.Month==month 
+                       && table.NGAY.Value.Year == DateTime.Now.Year)
+                       select (table);
+            return LICH.ToList();
+            
+        }
+        public List<DateTime> get_WorkDate(string MANV)
+        {
+            var LICH = from table in DB.LICHes
+                       where (table.MANV == MANV)
+                       select (table.NGAY.Value);
+            return LICH.ToList();
+
+        }
+        public List<LICH> ViewByDate(string MANV, DateTime dt)
+        {
+            var LICH = DB.LICHes.Where(rc=> rc.NGAY.Value == dt.Date && rc.MANV == MANV);
             return LICH.ToList();
         }
-        public void Them(string MALICH, string MANV, string MAPHG, string TENCV, string LOAICV, DateTime NGAY, DateTime THOIGIAN, string DIADIEM)
+        public void Them(string MALICH, string MANV, string MAPHG, string TENCV, 
+            string LOAICV, DateTime NGAY, DateTime THOIGIAN, string DIADIEM)
         {
             try
             {
@@ -48,15 +65,18 @@ namespace HRM_Hospital.BLL
                 lich.DIADIEM = DIADIEM;
                 DB.LICHes.InsertOnSubmit(lich);
                 DB.SubmitChanges();
-                MessageBox.Show("Thêm mới thông tin thành công.", "Thông báo.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Thêm mới thông tin thành công.", "Thông báo.", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information);
             }
             catch
             {
-                MessageBox.Show("Thêm mới thông tin thất bại.", "Thông báo.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Thêm mới thông tin thất bại.", "Thông báo.", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
             }
 
         }
-        public void Capnhat(string MALICH, string MANV, string MAPHG, string TENCV, string LOAICV, DateTime NGAY, DateTime THOIGIAN, string DIADIEM)
+        public void Capnhat(string MALICH, string MANV, string MAPHG, string TENCV, 
+            string LOAICV, DateTime NGAY, DateTime THOIGIAN, string DIADIEM)
         {
             try
             {
@@ -70,11 +90,13 @@ namespace HRM_Hospital.BLL
                 lich.THOIGIAN = THOIGIAN;
                 lich.DIADIEM = DIADIEM;
                 DB.SubmitChanges();
-                MessageBox.Show("Cập nhật thông tin thành công.", "Thông báo.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Cập nhật thông tin thành công.", "Thông báo.", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
             }
             catch
             {
-                MessageBox.Show("Cập nhật thông tin thành công.", "Thông báo.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Cập nhật thông tin thành công.", "Thông báo.", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information);
             }
         }
         public void Xoa(string MALICH)
@@ -84,12 +106,29 @@ namespace HRM_Hospital.BLL
                 var lich = DB.LICHes.FirstOrDefault(tk => tk.MALICH == MALICH);
                 DB.LICHes.DeleteOnSubmit(lich);
                 DB.SubmitChanges();
-                MessageBox.Show("Xóa thông tin khách hàng thành công.", "Thông báo", MessageBoxButtons.OK);
+                MessageBox.Show("Xóa thông tin thành công.", "Thông báo", MessageBoxButtons.OK);
             }
             catch
             {
-                MessageBox.Show("Xóa thông tin khách hàng thất bại.", "Thông báo", MessageBoxButtons.OK);
+                MessageBox.Show("Xóa thông tin thất bại.", "Thông báo", MessageBoxButtons.OK);
             }
+        }
+        public void Xoa_MANV(string MANV)
+        {
+            try
+            {
+                var lich = DB.LICHes.Where(li => li.MANV == MANV);
+                foreach (LICH li in lich)
+                    DB.LICHes.DeleteOnSubmit(li);
+                DB.SubmitChanges();
+                this.Status += "\nXóa trong bảng LICH: THÀNH CÔNG";
+            }
+            catch
+            {
+                this.Status += "\nXóa trong bảng LICH: THẤT BẠI";
+            }
+
         }
     }
 }
+
