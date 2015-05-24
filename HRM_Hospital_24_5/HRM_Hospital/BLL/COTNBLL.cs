607535c41220933195b04baf80486b0794aa24c7
quanlm.271 <quanlm.271@gmail.com>
2015-05-24 17:21:10 +0700

    Edit Ke Thua Them ham Xoa - khi xoa mot NV

--- HRM_Hospital_24_5/HRM_Hospital/BLL/COTNBLL.cs
+++ HRM_Hospital_24_5/HRM_Hospital/BLL/COTNBLL.cs
@@ -7,26 +7,60 @@ using HRM_Hospital.DAL;
 using System.Windows.Forms;
 namespace HRM_Hospital.BLL
 {
-    class COTNBLL
+    class COTNBLL:BLL
     {
-        QLNSDataContext DB = new QLNSDataContext();
+
         public void Them(String MANV, String MATN)
         {
+            try
+            {
                 COTN CTN = new COTN();
                 CTN.MANV = MANV;
                 CTN.MATN = MATN;
                 DB.COTNs.InsertOnSubmit(CTN);
                 DB.SubmitChanges();
+                MessageBox.Show("Thêm thông tin thành công.", "Thông báo", MessageBoxButtons.OK);
+            }
+            catch
+            {
+                MessageBox.Show("Thêm thông tin thất bại.", "Thông báo", MessageBoxButtons.OK);
+            }
            
           
         }
         public void Xoa(String MATN)
         {
-                var CTN = DB.COTNs.FirstOrDefault(tk => tk.MATN == MATN);
+            try
+            {
+                var CTN = DB.COTNs.FirstOrDefault(rc => rc.MATN == MATN);
                 DB.COTNs.DeleteOnSubmit(CTN);
                 DB.SubmitChanges();
-            
-        }
 
+                MessageBox.Show("Xóa thông tin thành công.", "Thông báo", MessageBoxButtons.OK);
+            }
+            catch
+            {
+                MessageBox.Show("Xóa thông tin thất bại.", "Thông báo", MessageBoxButtons.OK);
+            } 
+        }
+        public void Xoa_MANV(string MANV)
+        {
+            try
+            {
+                var CTN = DB.COTNs.Where(rc => rc.MANV == MANV);
+                foreach (COTN cotn in CTN)
+                {
+                    DB.COTNs.DeleteOnSubmit(cotn);
+                    DB.SubmitChanges();
+                    THANNHANBLL TN = new THANNHANBLL();
+                    TN.Xoa(cotn.MATN);
+                }
+                this.Status += "\nXóa trong Bảng COTN: THÀNH CÔNG";
+            }                  
+            catch
+            {
+                this.Status += "\nXóa trong Bảng COTN: THẤT BẠI";
+            }            
+        }
     }
 }
