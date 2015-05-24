fc7709e9ad449c3530eb2f994fa1c50ad9930539
quanlm.271 <quanlm.271@gmail.com>
2015-05-24 17:22:24 +0700

    - Sua Ham Set Ma - Them ham Xoa

--- HRM_Hospital_24_5/HRM_Hospital/BLL/DONXINBLL.cs
+++ HRM_Hospital_24_5/HRM_Hospital/BLL/DONXINBLL.cs
@@ -10,18 +10,24 @@ using System.Windows.Forms;
 
 namespace HRM_Hospital.BLL
 {
-    class DONXINBLL
+    class DONXINBLL: BLL
     {
-        QLNSDataContext DB = new QLNSDataContext();
-
         public List<DONXIN> LayTatCa()
         {
             return DB.DONXINs.ToList();
         }
 
-        public int set_MADONXIN()
+        public string set_MADONXIN()
         {
-            return DB.DONXINs.Count()+1;
+            string str;
+            var DX = DB.DONXINs.ToList();
+            if (DX.Count>0)
+            {
+                str = DX.Last().MADONXIN;
+                str = String.Concat(str.Where(char.IsDigit));
+                return "D" + (Int16.Parse(str) + 1);
+            }
+            return "D1";
         }
         public List<DONXIN> get_DONXIN(string MANV)
         {
@@ -32,7 +38,7 @@ namespace HRM_Hospital.BLL
             try
             {
                 DONXIN donxin = new DONXIN();
-                donxin.MADONXIN = this.set_MADONXIN().ToString();
+                donxin.MADONXIN = this.set_MADONXIN();
                 donxin.MANV = MANV;
                 donxin.LYDO = LYDO;
                 donxin.TUNGAY = TUNGAY;
@@ -41,13 +47,39 @@ namespace HRM_Hospital.BLL
                 donxin.NGAYLAPDON = NGAYLAPDON;
                 DB.DONXINs.InsertOnSubmit(donxin);
                 DB.SubmitChanges();
-                MessageBox.Show("Thêm mới thông tin thành công.", "Thông báo.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Thêm mới thông tin thành công.", "Thông báo.", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information);
             }
             catch
             {
-                MessageBox.Show("Thêm mới thông tin thất bại.", "Thông báo.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Thêm mới thông tin thất bại.", "Thông báo.", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information);
             }
-
+        }
+        public void Xoa(String MANV)
+        {    
+            try
+            {
+                var DX = (from table in DB.DONXINs
+                         where table.MANV == MANV
+                         select (table)).ToList();
+                foreach (DONXIN d in DX)
+                    DB.DONXINs.DeleteOnSubmit(d);
+                DB.SubmitChanges();
+                if (!onMinifrm)
+                    MessageBox.Show("Xoá thông tin thành công.", "Thông báo.", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                else
+                    this.Status += "\nXóa trong Bảng DONXINPHEP: THÀNH CÔNG";
+            }
+            catch
+            {
+                if (!onMinifrm)
+                    MessageBox.Show("Xóa thông tin thất bại.", "Thông báo.", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                else
+                    this.Status += "\nXóa trong Bảng DONXINPHEP: THẤT BẠI";
+            }
         }
 
     }
