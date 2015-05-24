ce11f7d795ef9fe92309b79b525f6c225df1d229
quanlm.271 <quanlm.271@gmail.com>
2015-05-24 17:19:06 +0700

    Class BLL cha

---
+++ HRM_Hospital_24_5/HRM_Hospital/BLL/BLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRM_Hospital.BLL;
+using HRM_Hospital.DAL;
+
+namespace HRM_Hospital.BLL
+{
+    class BLL
+    {
+        protected QLNSDataContext DB = new QLNSDataContext();
+        public string Status;
+        public Boolean onMinifrm = false;
+    }
+}
